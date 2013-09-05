8336ea3ccea9aa3e5012f02f4ceca31c7de52076
David Linus Briemann <dbriemann@gmail.com>
2013-09-05 16:06:26 +0200

    restructuring

--- BitBoard.hpp
+++ BitBoard.hpp
@@ -1,9 +1,8 @@
 #ifndef BITBOARD_HPP
 #define BITBOARD_HPP
 
-#include "Board.hpp"
 
-struct BitBoard : public Board {
+struct BitBoard {
     int32_t eval();
     uint8_t get(Index x, Index y) const;
     void set(Index x, Index y, uint8_t value);
---
+++ bb_definitions.hpp
@@ -0,0 +1,12 @@
+#ifndef BB_DEFINITIONS_HPP
+#define BB_DEFINITIONS_HPP
+
+#include <cstdlib>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64; //
+#define C64(constantU64) constantU64##ULL
+
+#endif // BB_DEFINITIONS_HPP
